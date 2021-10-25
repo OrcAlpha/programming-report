f062bed7e6fc473a38eff0d5ca8fd44b0f95c08b
Takeru <orcinus.orca.take@gmail.com>
2021-10-25 17:10:45 +0900

    ひとまず完成

--- newton.c
+++ newton.c
@@ -5,16 +5,14 @@ int n, nmax;
 double x, x2, dx, eps, fx, dfx;
 
 int main(void){
-	x = -1;
-	fx = x + cos(x);
-	dfx =1 - sin(x);
+	x = -1;/*自分で解に近いと思われる値を代入*/
 	eps = 0.0001;
 	n = 0;
 	nmax =10000 ;
 	
 	do{
-		fx = x + cos(x);
-		dfx = 1 - sin(x);
+		fx = x + cos(x);/*fxを手入力*/
+		dfx = 1 - sin(x);/*fxの微分を手入力*/
 		dx =  - fx / dfx;
 		x = x + dx;
 		n++;
