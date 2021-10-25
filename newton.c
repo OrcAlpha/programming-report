5efe899ce37baa724305fa2eb619f20d99a7a7a8
Takeru <orcinus.orca.take@gmail.com>
2021-10-25 17:07:47 +0900

    ひとまず完成

--- newton.c
+++ newton.c
@@ -8,21 +8,25 @@ int main(void){
 	x = -1;
 	fx = x + cos(x);
 	dfx =1 - sin(x);
-	eps = 0.5;
+	eps = 0.0001;
 	n = 0;
 	nmax =10000 ;
 	
 	do{
+		fx = x + cos(x);
+		dfx = 1 - sin(x);
 		dx =  - fx / dfx;
 		x = x + dx;
 		n++;
+
+		printf("%f\n" , x);
 		if(n == nmax){
 			printf("error");
 			break;
 		}
 
 	}
-	while(abs(dx) > eps && n <= nmax);
+	while(fabs(dx) > eps && n <= nmax);
 	printf("%f\n", x);
 
 		return 0;
